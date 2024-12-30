9b64579d06a450cb3e3243c01bfabcfe395e2f97
Ayush Tolani <ayushtolani1@gmail.com>
2024-12-30 18:15:09 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express=require('express');
 
 const app=express();
 const db = require('./db')
+require('dotenv').config();
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
+const port = process.env.port ||3000;
+
 app.get('/',function(req,res){
     res.send('Hare krishna , how can i help you?')
 })  
@@ -14,7 +17,6 @@ app.use('/menu',MenuItemRoutes);
 
 const personRoutes= require('./routes/person_routes');
 app.use('/person',personRoutes);
-
-app.listen(3000,()=>{ console.log('listening to server')})
+app.listen(port,()=>{ console.log('listening to server')})
 
 //comment added
